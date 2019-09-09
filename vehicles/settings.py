ccf30b2f2d207444549af8ebd309c7b3d5c8a1df
Omar <o.mohammed@morniksa.com>
2019-09-09 18:09:16 +0200

    dfg

--- vehicles/settings.py
+++ vehicles/settings.py
@@ -89,8 +89,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'vwhicles',
-            'USER': 'vwhiclesuser',
+            'NAME': 'vehicles',
+            'USER': 'vehiclesuser',
             'PASSWORD': '12345678',
             'HOST': 'localhost',
             'PORT': '5432',
