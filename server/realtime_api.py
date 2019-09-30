106f5cbf9d846b7038397c9105a89d039227b123
Omar <o.mohammed@morniksa.com>
2019-09-30 11:53:48 +0200

    fix

--- server/realtime_api.py
+++ server/realtime_api.py
@@ -23,8 +23,9 @@ class RealtimeApi:
             }
         }
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        try:
-            requests.post(url=REMOTE_URL, data=json.dumps(data), headers=headers)
-        except ConnectionError:
-            pass
+        requests.post(url=REMOTE_URL, data=json.dumps(data), headers=headers)
+        # try:
+        #     requests.post(url=REMOTE_URL, data=json.dumps(data), headers=headers)
+        # except ConnectionError:
+        #     pass
         # pdb.set_trace()
